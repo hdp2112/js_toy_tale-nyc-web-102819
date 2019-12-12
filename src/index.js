0eea241739c536c928e2f6dac86ef3f5f412ad03
hdp2112 <hdp2112@columbia.edu>
2019-12-12 15:42:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,72 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
+let toyList = document.querySelector('#toy-collection');
 
-// YOUR CODE HERE
+function swagarino(toyObj) {
+  let card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = ` 
+    <h2>${toyObj.name}</h2>
+    <img src="${toyObj.image}" class="toy-avatar" />
+    <p>${toyObj.likes} Likes </p>
+    <button class="like-btn" id="${toyObj.id}" data-likes = ${toyObj.likes}>Like <3</button>
+  `;
+  toyList.appendChild(card);
+}
+
+function createToy(newToy) {
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      accepts: "application/json"
+    },
+    body: JSON.stringify(newToy)})
+    .then(function(response) {return response.json()})
+    .then(swagarino);
+}
+
+document.body.addEventListener('click', function (e) {
+  console.log('Clicked!')
+  if (e.target.innerText === 'Like <3') {
+    console.log('Like button!!!!!')
+    e.target.dataset.likes = parseInt(e.target.dataset.likes) + 1
+    fetch(`http://localhost:3000/toys/1`, {
+      method: "PATCH",
+      headers: {
+      "Content-Type" : "application/json",
+      Accept: "application/json"
+      },
+      body: JSON.stringify({ 
+        "likes": `${e.target.dataset.likes}`
+      })
+    })
+
+    e.target.parentNode.querySelector('p').innerText = `${e.target.dataset.likes} Likes`
+  }
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:3000/toys")
+  .then(function(response) {return response.json()})
+  .then(function (toys) { 
+  toys.forEach(swagarino);
+
+  })
+})
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
-    // submit listener here
+    toyForm.addEventListener('submit', function(event) {
+      event.preventDefault()
+      let newToy = { name: `${event.target.name.value}`, image: `${event.target.image.value}`, likes: 0 };
+      createToy(newToy);
+    })
   } else {
     toyForm.style.display = 'none'
   }
 })
-
-
-// OR HERE!
